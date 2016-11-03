2ea37db13c963b917dfb3be44e3df92c6cdd264d
Foxscotch <foxscotch@gmail.com>
2016-11-03 17:04:33 -0500

    Progress on JS implementation

--- JavaScript/command-finder.js
+++ JavaScript/command-finder.js
@@ -1,11 +1,65 @@
+const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
+
 const jszip = require('jszip');
 
-const args = require('yargs')
+
+const args = getArgs();
+const commands = {};
+
+
+function searchScript(buffer) {}
+
+function searchFolder(filename) {
+  if (path.basename(filename) !== args.folder) {
+    commands[path.basename(filename)] = {};
+  }
+
+  fs.readdir(filename, (err, items) => {
+    if (err)
+      throw err;
+
+    for (let item of items) {
+      fs.stat(item, (err, stats) => {
+        if (err)
+          throw err;
+        
+        if (stats.isDirectory()) {
+          searchFolder(item);
+        } else if (path.extname(filename) === '.zip') {
+          searchZip(item);
+        } else {
+          searchScript(fs.createReadStream(item));
+        }
+      });
+    }
+  });
+}
+
+function searchZip(filename) {
+  fs.readFile(filename, (err, data) => {
+    if (err)
+      throw err;
+
+    jszip.loadAsync(data).then(zip => {
+      zip.forEach((relativePath, file) => {
+        if (!file.dir && path.extname(relativePath) === '.cs') {
+          searchScript(file.nodeStream());
+        }
+      });
+    });
+  });
+}
+
+
+function getArgs() {
+  return require('yargs')
   .usage('Usage: $0 -f [folder] -t [type] -o [out]')
   .options({
     f: {
       alias: 'folder',
+      coerce: f => path.isAbsolute(f) ? f : path.resolve(process.cwd(), f),
       default: './Add-Ons',
       describe: 'Add-Ons folder to search'
     },
@@ -22,3 +76,10 @@ const args = require('yargs')
     }
   })
   .argv;
+}
+
+function main() {
+  searchFolder(args.folder, commands);
+}
+
+main();
